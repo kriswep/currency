9b767fb6c2e8c79311d42bf3b5be60504cc4d2ba
kriswep <kriswep@wetainment.com>
2020-03-25 21:55:56 +0100

    fetch and display currencies

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useQuery } from 'react-query';
 
 export default function App() {
+  const [base, setBase] = useState('EUR');
+  const [target, setTarget] = useState('USD');
+
+  const { _, data, error } = useQuery(
+    ['latest', base, target],
+    fetchCurrencies,
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Choose currencies!</Text>
+      <TextInput
+        value={base}
+        onChangeText={text => setBase(text.toUpperCase())}
+      />
+      <TextInput
+        value={target}
+        onChangeText={text => setTarget(text.toUpperCase())}
+      />
+
+      {error && <Text>Uh Oh, an error happened...</Text>}
+      {data && <Text>{JSON.stringify(data)}</Text>}
     </View>
   );
 }
+
+const fetchCurrencies = async (_, base, target) => {
+  const res = await fetch(
+    `https://api.ratesapi.io/api/latest?base=${base}&symbols=${target}`,
+  );
+
+  return await res.json();
+};
 
 const styles = StyleSheet.create({
   container: {
