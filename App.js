732bf49b2c0873af1c780e1028a17ae3a39a6a36
kriswep <kriswep@wetainment.com>
2020-03-26 21:39:41 +0100

    calculate currencies

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { useQuery } from 'react-query';
 
@@ -6,24 +6,61 @@ export default function App() {
   const [base, setBase] = useState('EUR');
   const [target, setTarget] = useState('USD');
 
+  const [baseAmount, setBaseAmount] = useState('1');
+  const [targetAmount, setTargetAmount] = useState('0');
+
   const { _, data, error } = useQuery(
     ['latest', base, target],
     fetchCurrencies,
   );
+
+  useEffect(() => calculateFromBase(baseAmount), [data]);
+
+  const calculateFromBase = (number) => {
+    if (!isNaN(number) && data) {
+      setBaseAmount(number);
+      setTargetAmount((number * data.rates[target]).toFixed(2));
+    }
+  };
+  const calculateFromTarget = (number) => {
+    if (!isNaN(number) && data) {
+      setBaseAmount((number / data.rates[target]).toFixed(2));
+      setTargetAmount(number);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Choose currencies!</Text>
-      <TextInput
-        value={base}
-        onChangeText={text => setBase(text.toUpperCase())}
-      />
-      <TextInput
-        value={target}
-        onChangeText={text => setTarget(text.toUpperCase())}
-      />
-
       {error && <Text>Uh Oh, an error happened...</Text>}
-      {data && <Text>{JSON.stringify(data)}</Text>}
+
+      {data && (
+        <>
+          <View>
+            <TextInput
+              value={base}
+              onChangeText={(text) => setBase(text.toUpperCase())}
+            />
+            <TextInput
+              style={styles.currencyAmount}
+              keyboardType="numeric"
+              value={baseAmount}
+              onChangeText={calculateFromBase}
+            />
+          </View>
+          <View>
+            <TextInput
+              value={target}
+              onChangeText={(text) => setTarget(text.toUpperCase())}
+            />
+            <TextInput
+              style={styles.currencyAmount}
+              keyboardType="numeric"
+              value={targetAmount}
+              onChangeText={calculateFromTarget}
+            />
+          </View>
+        </>
+      )}
     </View>
   );
 }
@@ -39,8 +76,16 @@ const fetchCurrencies = async (_, base, target) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  currencyAmount: {
+    fontSize: 32,
+    padding: 12,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#000',
   },
 });
